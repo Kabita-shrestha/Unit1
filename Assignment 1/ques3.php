ae15f96f82f4e904c0cff6c02694b6e12eb6043c
Kabita-shrestha <kabitashrestha341@gmail.com>
2025-09-07 19:44:18 +0545

    Question3

---
+++ Assignment 1/ques3.php
@@ -0,0 +1,7 @@
+<!-- Write a program (constant.php) that defines a constant SITE_NAME = "Pokhara University" and prints it. -->
+
+<?php
+define("Site_Name","Pokhara University");
+
+echo Site_Name;
+?>
