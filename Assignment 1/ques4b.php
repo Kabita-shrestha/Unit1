8a19329ecf22fabd653ea24650a216f13673165a
Kabita-shrestha <kabitashrestha341@gmail.com>
2025-09-07 19:45:57 +0545

    Operators

---
+++ Assignment 1/ques4b.php
@@ -0,0 +1,42 @@
+ <!-- Initialize a variable and apply the following operators step by step:
+
+
++=
+
+-=
+
+*=
+
+/=
+
+%=
+
+
+Display the result after each operation. -->
+
+
+<?php
+
+$num = 10;
+echo "Initial value: $num <br>";
+
+
+$num += 5; 
+echo "After += 5: $num<br>";
+
+
+$num -= 3; 
+echo "After -= 3: $num<br>";
+
+
+$num *= 2; 
+echo "After *= 2: $num<br>";
+
+
+$num /= 4; 
+echo "After /= 4: $num<br>";
+
+
+$num %= 3;
+echo "After %= 3: $num<br>";
+?>
